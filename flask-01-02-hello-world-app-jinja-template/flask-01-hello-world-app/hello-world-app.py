116c856ea677e34c5f61915e359eea00c54a0c47
serife <serifesydn31000@gmail.com>
2023-05-09 17:03:15 +0300

    flask-task

--- flask-01-02-hello-world-app-jinja-template/flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-jinja-template/flask-01-hello-world-app/hello-world-app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def head():
-    return 'Hello world Oktay'
+    return 'Hello world serife'
 @app.route('/second')
 def second():
     return 'This is second page'
